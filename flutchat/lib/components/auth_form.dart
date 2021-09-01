9134b8ff54682dceeeb47430d7b3e3247497758e
JFmaia <jfmaia741@gmail.com>
2021-09-01 20:50:00 -0300

    Modified File
    Modificando o formulario de autenticação, para alternar entre login e signup.

--- flutchat/lib/components/auth_form.dart
+++ flutchat/lib/components/auth_form.dart
@@ -1,3 +1,4 @@
+import 'package:flutchat/models/auth_form_data.dart';
 import 'package:flutter/material.dart';
 
 class AuthForm extends StatefulWidget {
@@ -8,6 +9,13 @@ class AuthForm extends StatefulWidget {
 }
 
 class _AuthFormState extends State<AuthForm> {
+  final _formkey = GlobalKey<FormState>();
+  final _formData = AuthFormData();
+
+  void _submit() {
+    _formkey.currentState?.validate();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
@@ -16,26 +24,57 @@ class _AuthFormState extends State<AuthForm> {
         child: Padding(
           padding: const EdgeInsets.all(16),
           child: Form(
+            key: _formkey,
             child: Column(
               children: [
+                if (_formData.isSignup)
+                  TextFormField(
+                    key: ValueKey('name'),
+                    initialValue: _formData.name,
+                    onChanged: (name) => _formData.name = name,
+                    decoration: InputDecoration(labelText: "Nome"),
+                  ),
                 TextFormField(
-                  decoration: InputDecoration(labelText: "Nome"),
-                ),
-                TextFormField(
+                  key: ValueKey('email'),
+                  initialValue: _formData.email,
+                  onChanged: (email) => _formData.email = email,
                   decoration: InputDecoration(labelText: "E-mail"),
                 ),
                 TextFormField(
+                  key: ValueKey('password'),
+                  initialValue: _formData.password,
+                  onChanged: (password) => _formData.password = password,
                   obscureText: true,
                   decoration: InputDecoration(labelText: "Senha"),
                 ),
                 SizedBox(height: 12),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: Text("Entrar"),
+                Row(
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: _submit,
+                        child: Text(_formData.isLogin ? "Entrar" : "Cadastrar"),
+                      ),
+                    ),
+                  ],
                 ),
-                TextButton(
-                  onPressed: () {},
-                  child: Text("Criar uma nova conta?"),
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextButton(
+                        child: Text(
+                          _formData.isLogin
+                              ? "Criar uma nova conta?"
+                              : "Já possui conta?",
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _formData.toggleAuthMode();
+                          });
+                        },
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
