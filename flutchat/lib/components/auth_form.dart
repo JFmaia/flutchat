97bdcbccce4917daf31b082dfee7d368cad62847
JFmaia <jfmaia741@gmail.com>
2021-09-01 21:07:13 -0300

    Modified File
    Adicionando validação aos campos.

--- flutchat/lib/components/auth_form.dart
+++ flutchat/lib/components/auth_form.dart
@@ -33,12 +33,26 @@ class _AuthFormState extends State<AuthForm> {
                     initialValue: _formData.name,
                     onChanged: (name) => _formData.name = name,
                     decoration: InputDecoration(labelText: "Nome"),
+                    validator: (_name) {
+                      final name = _name ?? '';
+                      if (name.trim().length < 5) {
+                        return "Nome deve ter no mínimo 5 caracteres.";
+                      }
+                      return null;
+                    },
                   ),
                 TextFormField(
                   key: ValueKey('email'),
                   initialValue: _formData.email,
                   onChanged: (email) => _formData.email = email,
                   decoration: InputDecoration(labelText: "E-mail"),
+                  validator: (_email) {
+                    final email = _email ?? '';
+                    if (!email.contains('@')) {
+                      return "Email informado invalido!";
+                    }
+                    return null;
+                  },
                 ),
                 TextFormField(
                   key: ValueKey('password'),
@@ -46,6 +60,13 @@ class _AuthFormState extends State<AuthForm> {
                   onChanged: (password) => _formData.password = password,
                   obscureText: true,
                   decoration: InputDecoration(labelText: "Senha"),
+                  validator: (_password) {
+                    final password = _password ?? '';
+                    if (password.length < 6) {
+                      return "A senha deve ter no mínimo 6 caracteres.";
+                    }
+                    return null;
+                  },
                 ),
                 SizedBox(height: 12),
                 Row(
