4ec57fe13e57398a9c852dc5a7adef45690e7eb4
JFmaia <jfmaia741@gmail.com>
2021-09-01 21:21:45 -0300

    Modified File
    Criando função para ir np formulario epgar as informações coletadas pelo formulario.

--- flutchat/lib/pages/auth_page.dart
+++ flutchat/lib/pages/auth_page.dart
@@ -4,14 +4,22 @@ import 'package:flutter/material.dart';
 class AuthPage extends StatelessWidget {
   const AuthPage({Key? key}) : super(key: key);
 
+  void _handleSubmit(AuthFormData) {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
-      body: Center(
-        child: SingleChildScrollView(
-          child: AuthForm(),
-        ),
+      body: Stack(
+        children: [
+          Center(
+            child: SingleChildScrollView(
+              child: AuthForm(
+                onSubmit: _handleSubmit,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
