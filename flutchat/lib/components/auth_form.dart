da0ee161086752bd64b50e80ca65ab14a0802f5a
JFmaia <jfmaia741@gmail.com>
2021-09-03 10:13:29 -0300

    Modified File
     Adicionando uma nova condição na função de submit, além de criar uma função para pegar a imagem tirada pelo image_picker para formulário.

--- flutchat/lib/components/auth_form.dart
+++ flutchat/lib/components/auth_form.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+import 'package:flutchat/components/user_image_picker.dart';
 import 'package:flutchat/models/auth_form_data.dart';
 import 'package:flutter/material.dart';
 
@@ -16,94 +18,119 @@ class _AuthFormState extends State<AuthForm> {
   final _formkey = GlobalKey<FormState>();
   final _formData = AuthFormData();
 
+  void _handleImagePick(File image) {
+    _formData.image = image;
+  }
+
+  void _showError(String erro) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(erro),
+        backgroundColor: Colors.blueGrey,
+      ),
+    );
+  }
+
   void _submit() {
     final isValid = _formkey.currentState?.validate() ?? false;
     if (!isValid) return;
+
+    if (_formData.image == null && _formData.isSignup) {
+      return _showError("Imagem não selecionada!");
+    }
+
     widget.onSubmit(_formData);
   }
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Card(
-        margin: const EdgeInsets.all(20),
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Form(
-            key: _formkey,
-            child: Column(
-              children: [
-                if (_formData.isSignup)
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Card(
+          margin: const EdgeInsets.all(20),
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Form(
+              key: _formkey,
+              child: Column(
+                children: [
+                  if (_formData.isSignup)
+                    UserImagePicker(
+                      onImagePick: _handleImagePick,
+                    ),
+                  if (_formData.isSignup)
+                    TextFormField(
+                      key: ValueKey('name'),
+                      initialValue: _formData.name,
+                      onChanged: (name) => _formData.name = name,
+                      decoration: InputDecoration(labelText: "Nome"),
+                      validator: (_name) {
+                        final name = _name ?? '';
+                        if (name.trim().length < 5) {
+                          return "Nome deve ter no mínimo 5 caracteres.";
+                        }
+                        return null;
+                      },
+                    ),
                   TextFormField(
-                    key: ValueKey('name'),
-                    initialValue: _formData.name,
-                    onChanged: (name) => _formData.name = name,
-                    decoration: InputDecoration(labelText: "Nome"),
-                    validator: (_name) {
-                      final name = _name ?? '';
-                      if (name.trim().length < 5) {
-                        return "Nome deve ter no mínimo 5 caracteres.";
+                    key: ValueKey('email'),
+                    initialValue: _formData.email,
+                    onChanged: (email) => _formData.email = email,
+                    decoration: InputDecoration(labelText: "E-mail"),
+                    validator: (_email) {
+                      final email = _email ?? '';
+                      if (!email.contains('@')) {
+                        return "Email informado invalido!";
                       }
                       return null;
                     },
                   ),
-                TextFormField(
-                  key: ValueKey('email'),
-                  initialValue: _formData.email,
-                  onChanged: (email) => _formData.email = email,
-                  decoration: InputDecoration(labelText: "E-mail"),
-                  validator: (_email) {
-                    final email = _email ?? '';
-                    if (!email.contains('@')) {
-                      return "Email informado invalido!";
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  key: ValueKey('password'),
-                  initialValue: _formData.password,
-                  onChanged: (password) => _formData.password = password,
-                  obscureText: true,
-                  decoration: InputDecoration(labelText: "Senha"),
-                  validator: (_password) {
-                    final password = _password ?? '';
-                    if (password.length < 6) {
-                      return "A senha deve ter no mínimo 6 caracteres.";
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(height: 12),
-                Row(
-                  children: [
-                    Expanded(
-                      child: ElevatedButton(
-                        onPressed: _submit,
-                        child: Text(_formData.isLogin ? "Entrar" : "Cadastrar"),
-                      ),
-                    ),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Expanded(
-                      child: TextButton(
-                        child: Text(
-                          _formData.isLogin
-                              ? "Criar uma nova conta?"
-                              : "Já possui conta?",
+                  TextFormField(
+                    key: ValueKey('password'),
+                    initialValue: _formData.password,
+                    onChanged: (password) => _formData.password = password,
+                    obscureText: true,
+                    decoration: InputDecoration(labelText: "Senha"),
+                    validator: (_password) {
+                      final password = _password ?? '';
+                      if (password.length < 6) {
+                        return "A senha deve ter no mínimo 6 caracteres.";
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 12),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: ElevatedButton(
+                          onPressed: _submit,
+                          child:
+                              Text(_formData.isLogin ? "Entrar" : "Cadastrar"),
                         ),
-                        onPressed: () {
-                          setState(() {
-                            _formData.toggleAuthMode();
-                          });
-                        },
                       ),
-                    ),
-                  ],
-                ),
-              ],
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: TextButton(
+                          child: Text(
+                            _formData.isLogin
+                                ? "Criar uma nova conta?"
+                                : "Já possui conta?",
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _formData.toggleAuthMode();
+                            });
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
