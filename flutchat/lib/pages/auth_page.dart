b37d065f3cbfb04f52d6619cfbfb0576681049a2
JFmaia <jfmaia741@gmail.com>
2021-09-04 17:59:05 -0300

    Modified File
    Adicionando mecanismo de login e logout.

--- flutchat/lib/pages/auth_page.dart
+++ flutchat/lib/pages/auth_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutchat/components/auth_form.dart';
 import 'package:flutchat/core/models/auth_form_data.dart';
+import 'package:flutchat/core/services/auth/auth_mock_service.dart';
 import 'package:flutter/material.dart';
 
 class AuthPage extends StatefulWidget {
@@ -17,8 +18,18 @@ class _AuthPageState extends State<AuthPage> {
       setState(() => _isLoading = true);
       if (formData.isLogin) {
         // Login
+        await AuthMockService().login(
+          formData.email,
+          formData.password,
+        );
       } else {
         // Signup
+        await AuthMockService().signup(
+          formData.name,
+          formData.email,
+          formData.password,
+          formData.image,
+        );
       }
     } catch (error) {
       // Tratar error.
