d6d9bead4a1e23a0f0bafb7878d3e7c560ce1e4d
JFmaia <jfmaia741@gmail.com>
2021-09-04 09:11:04 -0300

    New Folder
    Criando pasta para os serviços, criando class abstrata e implemantando ela em auth_mock_service, nela foi codificado a logica para haver login, logout, signup da aplicação sem banco de dados.

---
+++ flutchat/lib/core/services/auth/auth_mock_service.dart
@@ -0,0 +1,56 @@
+import 'dart:async';
+import 'dart:io';
+import 'dart:math';
+
+import 'package:flutchat/core/models/chat_user.dart';
+import 'package:flutchat/core/services/auth/auth_service.dart';
+
+class AuthMockService implements AuthService {
+  static Map<String, ChatUser> _users = {};
+  static ChatUser? _currentUser;
+  static MultiStreamController<ChatUser?>? _controller;
+  static final _userStream = Stream<ChatUser?>.multi((controller) {
+    _controller = controller;
+    _updateUser(null);
+  });
+
+  ChatUser? get currentUser {
+    return null;
+  }
+
+  Stream<ChatUser?> get userChanges {
+    return _userStream;
+  }
+
+  Future<void> signup(
+    String name,
+    String email,
+    String password,
+    File image,
+  ) async {
+    final newUser = ChatUser(
+      id: Random().nextDouble().toString(),
+      name: name,
+      email: email,
+      imageUrl: image.path,
+    );
+    _users.putIfAbsent(email, () => newUser);
+    _updateUser(newUser);
+  }
+
+  Future<void> login(
+    String email,
+    String password,
+  ) async {
+    _updateUser(_users[email]);
+  }
+
+  Future<void> logout() async {
+    _updateUser(null);
+  }
+
+  static void _updateUser(ChatUser? user) {
+    _currentUser = user;
+    _controller?.add(_currentUser);
+  }
+}
