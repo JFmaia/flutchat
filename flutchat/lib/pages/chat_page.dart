87449023eae97bd4f41ac5aeca3b62cb26afd3a1
JFmaia <jfmaia741@gmail.com>
2021-09-04 18:00:16 -0300

    New File
    Fazendo tela do chat, provisorio, um text e logout.

---
+++ flutchat/lib/pages/chat_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutchat/core/services/auth/auth_mock_service.dart';
+import 'package:flutter/material.dart';
+
+class ChatPage extends StatefulWidget {
+  const ChatPage({Key? key}) : super(key: key);
+
+  @override
+  _ChatPageState createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Olá User"),
+            TextButton(
+              child: Text("Logout"),
+              onPressed: () {
+                AuthMockService().logout();
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
