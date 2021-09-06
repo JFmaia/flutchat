8a356b27be2187388a15e0c98e5423f9c1e94530
JFmaia <jfmaia741@gmail.com>
2021-09-06 10:08:02 -0300

    Modified File
    Colocando texto de exemplo.

--- flutchat/lib/components/new_message.dart
+++ flutchat/lib/components/new_message.dart
@@ -5,6 +5,8 @@ class NewMessage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Text("Nova menssagem"),
+    );
   }
 }
