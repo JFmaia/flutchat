67a86bcea73d6590540e1693452e0f092376604f
JFmaia <jfmaia741@gmail.com>
2021-09-06 09:37:36 -0300

    Modified File
    Trocando o AuthMockService por AuthService.

--- flutchat/lib/pages/auth_page.dart
+++ flutchat/lib/pages/auth_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutchat/components/auth_form.dart';
 import 'package:flutchat/core/models/auth_form_data.dart';
-import 'package:flutchat/core/services/auth/auth_mock_service.dart';
+import 'package:flutchat/core/services/auth/auth_service.dart';
 import 'package:flutter/material.dart';
 
 class AuthPage extends StatefulWidget {
@@ -18,13 +18,13 @@ class _AuthPageState extends State<AuthPage> {
       setState(() => _isLoading = true);
       if (formData.isLogin) {
         // Login
-        await AuthMockService().login(
+        await AuthService().login(
           formData.email,
           formData.password,
         );
       } else {
         // Signup
-        await AuthMockService().signup(
+        await AuthService().signup(
           formData.name,
           formData.email,
           formData.password,
