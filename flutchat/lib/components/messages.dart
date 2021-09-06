f675aa9199e000639066232ba1ea42885ea4f479
JFmaia <jfmaia741@gmail.com>
2021-09-06 09:46:10 -0300

    Modified File
    Modificando o nome do arquivo.

--- flutchat/lib/components/messages.dart
+++ flutchat/lib/components/messages.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class Message extends StatelessWidget {
-  const Message({Key? key}) : super(key: key);
+class Messages extends StatelessWidget {
+  const Messages({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
