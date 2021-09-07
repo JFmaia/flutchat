86dd3362b94b01d503767dbd483f29b9ec872256
JFmaia <jfmaia741@gmail.com>
2021-09-07 09:49:48 -0300

    Modified File
    Implementando na class ChatMockService o ChatService, e implementando os metodos.

---
+++ flutchat/lib/core/services/chat/chat_mock_service.dart
@@ -0,0 +1,65 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:flutchat/core/models/chat_message.dart';
+import 'package:flutchat/core/models/chat_user.dart';
+
+import 'chat_service.dart';
+
+class ChatMockService implements ChatService {
+  static final List<ChatMessage> _msgs = [
+    ChatMessage(
+      id: '1',
+      text: 'Bom dia!',
+      createdAt: DateTime.now(),
+      userId: '12345',
+      userName: 'José',
+      userImageURL: 'assets/images/avatar.png',
+    ),
+    ChatMessage(
+      id: '2',
+      text: 'Bom dia, teremos reunião hoje?',
+      createdAt: DateTime.now(),
+      userId: '123456',
+      userName: 'Carla',
+      userImageURL: 'assets/images/avatar.png',
+    ),
+    ChatMessage(
+      id: '3',
+      text: 'Sim. Pode ser agora!',
+      createdAt: DateTime.now(),
+      userId: '1234567',
+      userName: 'Matheus',
+      userImageURL: 'assets/images/avatar.png',
+    ),
+  ];
+  static MultiStreamController<List<ChatMessage>>? _controller;
+  static final _msgsStream = Stream<List<ChatMessage>>.multi(
+    (controller) {
+      _controller = controller;
+      controller.add(_msgs);
+    },
+  );
+
+  Stream<List<ChatMessage>> messagesStream() {
+    return _msgsStream;
+  }
+
+  Future<ChatMessage> save(String text, ChatUser user) async {
+    final newMessage = ChatMessage(
+      id: Random().nextDouble().toString(),
+      text: text,
+      createdAt: DateTime.now(),
+      userId: user.id,
+      userName: user.name,
+      userImageURL: user.imageUrl,
+    );
+
+    //Adicionando menssagem na lista.
+    _msgs.add(newMessage);
+    //Adicionando a lista de menssagens para a Stream.
+    _controller!.add(_msgs);
+
+    return newMessage;
+  }
+}
