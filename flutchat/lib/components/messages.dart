57921e458c3ec653e770d4ced0feefd9ea3eb531
JFmaia <jfmaia741@gmail.com>
2021-09-10 16:16:05 -0300

    Modified File
    Adicionando o MessageBubble.

--- flutchat/lib/components/messages.dart
+++ flutchat/lib/components/messages.dart
@@ -1,4 +1,6 @@
+import 'package:flutchat/components/message_bubble.dart';
 import 'package:flutchat/core/models/chat_message.dart';
+import 'package:flutchat/core/services/auth/auth_service.dart';
 import 'package:flutchat/core/services/chat/chat_service.dart';
 import 'package:flutter/material.dart';
 
@@ -7,6 +9,7 @@ class Messages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final currentUser = AuthService().currentUser;
     return StreamBuilder<List<ChatMessage>>(
       stream: ChatService().messagesStream(),
       builder: (ctx, snapshot) {
@@ -23,7 +26,11 @@ class Messages extends StatelessWidget {
           return ListView.builder(
             reverse: true,
             itemCount: msgs.length,
-            itemBuilder: (ctx, i) => Text(msgs[i].text),
+            itemBuilder: (ctx, i) => MessageBubble(
+              key: ValueKey(msgs[i].id),
+              message: msgs[i],
+              belongsToMe: currentUser?.id == msgs[i].userId,
+            ),
           );
         }
       },
