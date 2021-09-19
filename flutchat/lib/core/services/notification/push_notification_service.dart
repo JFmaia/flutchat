70918dee9473fcf66cfc5f5e04df709165023e5a
JFmaia <jfmaia741@gmail.com>
2021-09-19 08:07:32 -0300

    New File
    Criando um serviço para as notificações, usando ChangeNotifier, então o provider vai ser utilizado.

---
+++ flutchat/lib/core/services/notification/push_notification_service.dart
@@ -0,0 +1,19 @@
+import 'package:flutchat/core/models/chat_notification.dart';
+import 'package:flutter/cupertino.dart';
+
+class PushNotificationService with ChangeNotifier {
+  List<ChatNotification> _items = [];
+
+  List<ChatNotification> get items {
+    return [..._items];
+  }
+
+  void add(ChatNotification notification) {
+    _items.add(notification);
+    notifyListeners();
+  }
+
+  void remove(int i) {
+    _items.removeAt(i);
+  }
+}
