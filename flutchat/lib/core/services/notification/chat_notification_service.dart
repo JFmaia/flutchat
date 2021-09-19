d7d7850d2b9b1217f6b35a59b13b3a2e00ac2c0f
JFmaia <jfmaia741@gmail.com>
2021-09-19 09:21:48 -0300

    Modified File
    Criando metodo para retornar o numero de notificações.

--- flutchat/lib/core/services/notification/chat_notification_service.dart
+++ flutchat/lib/core/services/notification/chat_notification_service.dart
@@ -8,6 +8,10 @@ class ChatNotificationService with ChangeNotifier {
     return [..._items];
   }
 
+  int get itemsCount {
+    return _items.length;
+  }
+
   void add(ChatNotification notification) {
     _items.add(notification);
     notifyListeners();
