bbb2656b91db85956d8ff9fd8b060aa115712d40
JFmaia <jfmaia741@gmail.com>
2021-09-21 18:18:33 -0300

    Modified File
    Criando os conversores para refatorar o codigo.

--- flutchat/lib/core/services/chat/chat_firebase_service.dart
+++ flutchat/lib/core/services/chat/chat_firebase_service.dart
@@ -12,29 +12,53 @@ class ChatFirebaseService implements ChatService {
 
   Future<ChatMessage?> save(String text, ChatUser user) async {
     final store = FirebaseFirestore.instance;
-
-    // ChatMessage => Map<String, dynamic>
-    final docRef = await store.collection('chat').add(
-      {
-        'text': text,
-        'createdAt': DateTime.now().toIso8601String(),
-        'userId': user.id,
-        'userName': user.name,
-        'userImageURL': user.imageUrl,
-      },
+    final msg = ChatMessage(
+      id: '',
+      text: text,
+      createdAt: DateTime.now(),
+      userId: user.id,
+      userName: user.name,
+      userImageURL: user.imageUrl,
     );
+    // ChatMessage => Map<String, dynamic>
+    final docRef = await store
+        .collection('chat')
+        .withConverter(
+          fromFirestore: _fromFirestore,
+          toFirestore: _toFirestore,
+        )
+        .add(msg);
 
     final doc = await docRef.get();
-    final data = doc.data()!;
-    // Map<String, dynamic> => ChatMessage
+    return doc.data()!;
+  }
 
+  // ChatMessage => Map<String, dynamic>
+  Map<String, dynamic> _toFirestore(
+    ChatMessage msg,
+    SetOptions? setOptions,
+  ) {
+    return {
+      'text': msg.text,
+      'createdAt': msg.createdAt.toIso8601String(),
+      'userId': msg.userId,
+      'userName': msg.userName,
+      'userImageURL': msg.userImageURL,
+    };
+  }
+
+  // Map<String, dynamic> => ChatMessage
+  ChatMessage _fromFirestore(
+    DocumentSnapshot<Map<String, dynamic>> doc,
+    SnapshotOptions? options,
+  ) {
     return ChatMessage(
       id: doc.id,
-      text: data['text'],
-      createdAt: DateTime.parse(data['createdAt']),
-      userId: data['userId'],
-      userName: data['userName'],
-      userImageURL: data['userImageURL'],
+      text: doc['text'],
+      createdAt: DateTime.parse(doc['createdAt']),
+      userId: doc['userId'],
+      userName: doc['userName'],
+      userImageURL: doc['userImageURL'],
     );
   }
 }
