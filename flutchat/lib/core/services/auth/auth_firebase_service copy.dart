77992d8edb012de725a2cba4d5b3b36827c81a61
JFmaia <jfmaia741@gmail.com>
2021-09-20 21:58:04 -0300

    Modified File
    Fazendo a strem receber um novo user, esse user foi pego do firebase, fazendo o logout e implementando a metade do signup.

--- flutchat/lib/core/services/auth/auth_firebase_service copy.dart
+++ flutchat/lib/core/services/auth/auth_firebase_service copy.dart
@@ -1,14 +1,20 @@
 import 'dart:async';
 import 'dart:io';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutchat/core/models/chat_user.dart';
 import 'package:flutchat/core/services/auth/auth_service.dart';
 
 class AuthFirebaseService implements AuthService {
   static ChatUser? _currentUser;
-  static MultiStreamController<ChatUser?>? _controller;
-  static final _userStream = Stream<ChatUser?>.multi((controller) {
-    _controller = controller;
-  });
+  static final _userStream = Stream<ChatUser?>.multi(
+    (controller) async {
+      final authChanges = FirebaseAuth.instance.authStateChanges();
+      await for (final user in authChanges) {
+        _currentUser = user == null ? null : _toChatUser(user);
+        controller.add(_currentUser);
+      }
+    },
+  );
 
   ChatUser? get currentUser {
     return _currentUser;
@@ -23,12 +29,34 @@ class AuthFirebaseService implements AuthService {
     String email,
     String password,
     File? image,
-  ) async {}
+  ) async {
+    final auth = FirebaseAuth.instance;
+    UserCredential credential = await auth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+
+    if (credential.user == null) return;
+
+    credential.user?.updateDisplayName(name);
+    // credential.user?.updatePhotoURL(photoURL);
+  }
 
   Future<void> login(
     String email,
     String password,
   ) async {}
 
-  Future<void> logout() async {}
+  Future<void> logout() async {
+    FirebaseAuth.instance.signOut();
+  }
+
+  static ChatUser _toChatUser(User user) {
+    return ChatUser(
+      id: user.uid,
+      name: user.displayName ?? user.email!.split('@')[0],
+      email: user.email!,
+      imageUrl: user.photoURL ?? "assets/images/avatar.png",
+    );
+  }
 }
