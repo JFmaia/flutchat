1a6df8440c0e9bf1a30a22eda497798594c9841a
JFmaia <jfmaia741@gmail.com>
2021-09-21 17:47:09 -0300

    Modified File
    Modificando o metodo para adicionar a menssagem no banco de dados.

--- flutchat/lib/core/services/chat/chat_firebase_service.dart
+++ flutchat/lib/core/services/chat/chat_firebase_service.dart
@@ -1,1 +1,29 @@
+import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutchat/core/models/chat_message.dart';
+import 'package:flutchat/core/models/chat_user.dart';
 
+import 'chat_service.dart';
+
+class ChatFirebaseService implements ChatService {
+  Stream<List<ChatMessage>> messagesStream() {
+    return Stream<List<ChatMessage>>.empty();
+  }
+
+  Future<ChatMessage?> save(String text, ChatUser user) async {
+    final store = FirebaseFirestore.instance;
+
+    // ChatMessage => Map<String, dynamic>
+    store.collection('chat').add(
+      {
+        'text': text,
+        'createdAt': DateTime.now().toIso8601String(),
+        'userId': user.id,
+        'userName': user.name,
+        'userImageURL': user.imageUrl,
+      },
+    );
+
+    return null;
+  }
+}
