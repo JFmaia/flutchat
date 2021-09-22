7f191e9bd1345576853c088940f36de78855a573
JFmaia <jfmaia741@gmail.com>
2021-09-22 16:02:19 -0300

    Modified File
    Criando alguns do metodos que serão utilizados para fazer o Pushnotifications.

--- flutchat/lib/core/services/notification/chat_notification_service.dart
+++ flutchat/lib/core/services/notification/chat_notification_service.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutchat/core/models/chat_notification.dart';
 import 'package:flutter/cupertino.dart';
 
@@ -20,5 +21,31 @@ class ChatNotificationService with ChangeNotifier {
   void remove(int i) {
     _items.removeAt(i);
     notifyListeners();
+  }
+
+  // Push Notification
+  Future<void> init() async {
+    await _configureForeground();
+  }
+
+  Future<bool> get _isAuthorized async {
+    final messaging = FirebaseMessaging.instance;
+    final settigns = await messaging.requestPermission();
+
+    return settigns.authorizationStatus == AuthorizationStatus.authorized;
+  }
+
+  Future<void> _configureForeground() async {
+    if (await _isAuthorized) {
+      FirebaseMessaging.onMessage.listen((msg) {
+        if (msg.notification == null) return;
+        add(
+          ChatNotification(
+            title: msg.notification!.title ?? 'Não informado!',
+            body: msg.notification!.body ?? 'Não informado!',
+          ),
+        );
+      });
+    }
   }
 }
