eb7838ec9bbe51de8674d29165f431c6e34890f0
JFmaia <jfmaia741@gmail.com>
2021-09-22 16:32:23 -0300

    Modified File
    Criando alguns do metodos de para as menssagens de background e depois da aplicação fechar, além de separar o add da menssagem.

--- flutchat/lib/core/services/notification/chat_notification_service.dart
+++ flutchat/lib/core/services/notification/chat_notification_service.dart
@@ -26,6 +26,8 @@ class ChatNotificationService with ChangeNotifier {
   // Push Notification
   Future<void> init() async {
     await _configureForeground();
+    await _configureBackground();
+    await _configureTerminaded();
   }
 
   Future<bool> get _isAuthorized async {
@@ -37,15 +39,31 @@ class ChatNotificationService with ChangeNotifier {
 
   Future<void> _configureForeground() async {
     if (await _isAuthorized) {
-      FirebaseMessaging.onMessage.listen((msg) {
-        if (msg.notification == null) return;
-        add(
-          ChatNotification(
-            title: msg.notification!.title ?? 'Não informado!',
-            body: msg.notification!.body ?? 'Não informado!',
-          ),
-        );
-      });
+      FirebaseMessaging.onMessage.listen(_messageHandler);
     }
+  }
+
+  Future<void> _configureBackground() async {
+    if (await _isAuthorized) {
+      FirebaseMessaging.onMessageOpenedApp.listen(_messageHandler);
+    }
+  }
+
+  Future<void> _configureTerminaded() async {
+    if (await _isAuthorized) {
+      RemoteMessage? initialMsg =
+          await FirebaseMessaging.instance.getInitialMessage();
+      _messageHandler(initialMsg);
+    }
+  }
+
+  void _messageHandler(RemoteMessage? msg) {
+    if (msg == null || msg.notification == null) return;
+    add(
+      ChatNotification(
+        title: msg.notification!.title ?? 'Não informado!',
+        body: msg.notification!.body ?? 'Não informado!',
+      ),
+    );
   }
 }
