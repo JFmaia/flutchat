f729d187d6d8f8e9abb4febb536411f66b58674d
JFmaia <jfmaia741@gmail.com>
2021-09-22 16:01:18 -0300

    Modified File
    Inicializando o as pushnotifications.

--- flutchat/lib/pages/auth_or_app_page.dart
+++ flutchat/lib/pages/auth_or_app_page.dart
@@ -1,9 +1,11 @@
 import 'package:flutchat/core/models/chat_user.dart';
 import 'package:flutchat/core/services/auth/auth_service.dart';
+import 'package:flutchat/core/services/notification/chat_notification_service.dart';
 import 'package:flutchat/pages/chat_page.dart';
 import 'package:flutchat/pages/loading_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 
 import 'auth_page.dart';
 
@@ -12,6 +14,7 @@ class AuthOrAppPage extends StatelessWidget {
 
   Future<void> init(BuildContext context) async {
     await Firebase.initializeApp();
+    Provider.of<ChatNotificationService>(context, listen: false).init();
   }
 
   @override
